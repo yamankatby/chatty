4399997b4b330970d9319db5d91d28304f49d837
Yaman KATBY <m.yaman.katby@gmail.com>
2021-09-17 12:44:54 +0300

    🚧 Work on creating chat

--- screens/ChatList.js
+++ screens/ChatList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Text, View } from "react-native";
 import {
   List,
@@ -10,9 +10,38 @@ import {
   Button,
   TextInput,
 } from "react-native-paper";
+import firebase from "firebase/app";
+import { useNavigation } from "@react-navigation/core";
 
 const ChatList = () => {
   const [isDialogVisible, setIsDialogVisible] = useState(false);
+
+  const [email, setEmail] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      setEmail(user?.email ?? "");
+    });
+  }, []);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigation = useNavigation();
+
+  const createChat = async () => {
+    if (!email || !userEmail) return;
+    setIsLoading(true);
+    await firebase
+      .firestore()
+      .collection("chats")
+      .add({
+        users: [email, userEmail],
+      });
+    setIsLoading(false);
+    setIsDialogVisible(false);
+    navigation.navigate("Chat");
+  };
 
   return (
     <View style={{ flex: 1 }}>
@@ -30,11 +59,17 @@ const ChatList = () => {
         >
           <Dialog.Title>New Chat</Dialog.Title>
           <Dialog.Content>
-            <TextInput label="Enter user email" />
+            <TextInput
+              label="Enter user email"
+              value={userEmail}
+              onChangeText={(text) => setUserEmail(text)}
+            />
           </Dialog.Content>
           <Dialog.Actions>
             <Button onPress={() => setIsDialogVisible(false)}>Cancel</Button>
-            <Button>Save</Button>
+            <Button onPress={() => createChat()} loading={isLoading}>
+              Save
+            </Button>
           </Dialog.Actions>
         </Dialog>
       </Portal>
--- screens/Settings.js
+++ screens/Settings.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 import { Avatar, Title, Subheading, Button } from "react-native-paper";
 import firebase from "firebase/app";
 
 const Settings = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      setName(user?.displayName ?? "");
+      setEmail(user?.email ?? "");
+    });
+  }, []);
+
   return (
     <View style={{ alignItems: "center", marginTop: 16 }}>
-      <Avatar.Text label="UN" />
-      <Title>User Name</Title>
-      <Subheading>user@name.com</Subheading>
+      <Avatar.Text
+        label={name.split(" ").reduce((prev, current) => prev + current[0], "")}
+      />
+      <Title>{name}</Title>
+      <Subheading>{email}</Subheading>
       <Button onPress={() => firebase.auth().signOut()}>Sign Out</Button>
     </View>
   );
